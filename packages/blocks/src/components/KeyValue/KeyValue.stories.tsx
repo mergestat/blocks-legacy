e66c2f35e662adceef3dd0b1bdb539b3a1950de2
oleg-serf <olegserfcompany@gmail.com>
2022-04-13 08:09:47 -0400

    feat: implement 'KeyValue' component, #4

---
+++ packages/blocks/src/components/KeyValue/KeyValue.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { KeyValue } from './KeyValue';
+
+export default {
+  title: 'Molecules/KeyValue',
+  component: KeyValue,
+  argTypes: {},
+} as ComponentMeta<typeof KeyValue >;
+
+const Template: ComponentStory<typeof KeyValue> = (args) => <KeyValue {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "Key",
+  value:"Value"
+};
