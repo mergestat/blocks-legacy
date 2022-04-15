8d871455c67cccc87a60ed61f4eadfbb42c6bab3
oleg-serf <olegserfcompany@gmail.com>
2022-04-15 12:18:19 -0400

    fix: LogBox styles

--- packages/blocks/src/components/LogBox/LogBox.tsx
+++ packages/blocks/src/components/LogBox/LogBox.tsx
@@ -1,20 +1,22 @@
-import { ClipboardIcon } from '@mergestat/icons';
 import React from 'react';
+import { ClipboardIcon } from '@mergestat/icons';
 
 type LogBoxProps = {
   data: { massage: string | React.ReactNode }[];
-  onClick?: ([]) => void;
+  onClick?: () => void;
 };
 
 export const LogBox: React.FC<LogBoxProps> = ({ data, onClick }) => {
   return (
-    <div className="bg-gray-800 text-white p-10 rounded">
+    <div className="t-log-box">
       {data.map((item, index) => (
         <div key={`key_${index}`}>{item.massage}</div>
       ))}
       <button
-        className="absolute top-10 right-10 flex gap-1.5 items-center bg-gray-700 rounded p-2"
-        onClick={() => onClick && onClick(data)}
+        className="absolute top-10 right-10 flex gap-1.5 items-center bg-gray-700 rounded p-3"
+        onClick={() => {
+          if (onClick) onClick();
+        }}
       >
         <ClipboardIcon className="t-icon" /> Copy
       </button>
