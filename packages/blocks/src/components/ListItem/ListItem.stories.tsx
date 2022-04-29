3682024d0bc23573a1468a0687ee16f14625e492
oleg-serf <olegserfcompany@gmail.com>
2022-04-29 09:02:07 -0400

    feat(blocks): add new props for ListItem component

--- packages/blocks/src/components/ListItem/ListItem.stories.tsx
+++ packages/blocks/src/components/ListItem/ListItem.stories.tsx
@@ -16,4 +16,7 @@ Default.args = {
   title: "Repository Name",
   subline: "Subline",
   startIcon: <RepositoryIcon className="t-icon"/>,
+  className: "p-2 border-b",
+  onClick: () => console.log("click"),
+  onCloseClick: () => console.log("close click")
 }
--- packages/blocks/src/components/ListItem/ListItem.tsx
+++ packages/blocks/src/components/ListItem/ListItem.tsx
@@ -1,37 +1,53 @@
 import React from 'react';
 import { TrashIcon } from '@mergestat/icons';
+import cx from 'classnames';
 
+import classNames from 'classnames';
 type ListItemProps = {
   title: string;
   subline?: string;
   startIcon?: React.ReactNode;
+  className?: string;
   onClick?: () => void;
+  onCloseClick?: () => void;
 };
 
 export const ListItem: React.FC<ListItemProps> = ({
   title,
   subline,
   startIcon,
+  className,
   onClick,
+  onCloseClick,
 }) => {
   return (
-    <div className="w-full">
-      <div className="w-full t-list-item-container">
-        <div className="t-left-side">
-          {startIcon && (
-            <button className="t-list-button">
-              {startIcon}
-            </button>
-          )}
-          <div>
-            <h3>{title}</h3>
-            {subline && <p>{subline}</p>}
+    <div
+      className={cx('t-list-item-container', {
+        [className]: !!className,
+      })}
+      onClick={(e) => {
+        e.preventDefault()
+        const element = e.target as HTMLElement;
+        if (['svg', 'path', 'button'].includes(element.tagName)) { 
+          return;
+        }
+        onClick();
+      }}
+    >
+      <div className="t-left-side">
+        {startIcon && (
+          <div className="t-list-button">
+            {startIcon}
           </div>
-        </div>
-        <div className="t-right-side">
-          <TrashIcon className="t-icon" onClick={onClick} />
+        )}
+        <div>
+          <h3>{title}</h3>
+          {subline && <p>{subline}</p>}
         </div>
       </div>
+      <button className="t-right-side" onClick={onCloseClick}>
+        <TrashIcon className="t-icon" />
+      </button>
     </div>
   );
 };
