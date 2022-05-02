a67d9fa3769bbb50091a7505395dcc39e46e45b4
oleg-serf <olegserfcompany@gmail.com>
2022-05-02 11:32:37 -0400

    fix(blocks): add props className for InlineBanner component

--- packages/blocks/src/components/InlineBanner/InlineBanner.tsx
+++ packages/blocks/src/components/InlineBanner/InlineBanner.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import cx from 'classnames';
 
-type InlineBannerProps = {};
+type InlineBannerProps = {
+  className?: string;
+};
 
-export const InlineBanner: React.FC<InlineBannerProps> = ({ children }) => {
+export const InlineBanner: React.FC<InlineBannerProps> = ({ className, children }) => {
   return (
-    <div className="p-3 bg-gray-100 text-center">
+    <div className={cx("p-3 bg-gray-100 text-center", { [className]: !!className })}>
       <p className="t-inline-banner">
         {children && children}
       </p>
