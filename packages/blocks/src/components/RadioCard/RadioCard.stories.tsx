c9c4d5f39d320ecdcd2ca9196cde7984fbcdfecb
Emily <emily.vandewalle@hotmail.com>
2022-05-13 15:35:31 +0200

    radio card styling improvements

--- packages/blocks/src/components/RadioCard/RadioCard.stories.tsx
+++ packages/blocks/src/components/RadioCard/RadioCard.stories.tsx
@@ -13,14 +13,14 @@ const Template: ComponentStory<typeof RadioCard> = (args) => <RadioCard {...args
 
 export const Default = Template.bind({});
 Default.args = {
-  startIcon: <GithubIcon className="t-icon" />,
+  startIcon: <GithubIcon className="t-icon t-icon-default" />,
   label: "Option",
   isSelected:false
 }
 
 export const Selected = Template.bind({});
 Selected.args = {
-  startIcon: <GithubIcon className="t-icon" />,
+  startIcon: <GithubIcon className="t-icon t-icon-primary" />,
   label: "Option",
   isSelected: true
 }
