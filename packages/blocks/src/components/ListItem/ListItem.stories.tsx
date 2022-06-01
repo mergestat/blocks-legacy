4e60937682d9d2d8081c18e2d9401f571c8f5fed
Emily <emily.vandewalle@hotmail.com>
2022-06-01 13:35:58 +0200

    Use colored box inside list item + small improvements

--- packages/blocks/src/components/ListItem/ListItem.stories.tsx
+++ packages/blocks/src/components/ListItem/ListItem.stories.tsx
@@ -16,7 +16,7 @@ Default.args = {
   title: "Repository Name",
   subline: "Subline",
   startIcon: <RepositoryIcon className="t-icon"/>,
-  className: "p-2 border-b",
+  className: "px-3 py-4 border-b",
   onClick: () => console.log("click"),
   onCloseClick: () => console.log("close click")
 }
