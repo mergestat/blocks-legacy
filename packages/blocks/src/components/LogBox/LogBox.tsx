e849ad3c20ec78b5c8c474f199e154b6721cabad
Emily <emily.vandewalle@hotmail.com>
2022-10-13 18:13:47 +0200

    feat: update default logbox text color

--- packages/blocks/src/components/LogBox/LogBox.tsx
+++ packages/blocks/src/components/LogBox/LogBox.tsx
@@ -12,7 +12,7 @@ type LogBoxProps = {
 
 export const LogBox: React.FC<LogBoxProps> = ({ logs, className, copyBtnClassName, onCopy }) => {
   return (
-    <div className={cx("t-log-box bg-gray-800 text-white rounded-md font-mono text-xs leading-6", { [className]: !!className })}>
+    <div className={cx("t-log-box bg-gray-800 text-gray-200 rounded-md font-mono text-xs leading-6", { [className]: !!className })}>
       {logs.map((log, index) => (
         <p key={index}>
           {log}
