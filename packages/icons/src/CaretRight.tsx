d1e215c7e8d1923f57b9668c7dfe4176140fa636
Patrick DeVivo <patrick.devivo@gmail.com>
2022-11-09 09:29:41 -0500

    chore: update deps and fix a couple lint items

--- packages/icons/src/CaretRight.tsx
+++ packages/icons/src/CaretRight.tsx
@@ -5,7 +5,7 @@ import type { IconProps } from './index';
 function SvgCaretRight(props: IconProps) {
   return (
     <Icon {...props}>
-      <path d="M9 7L15 12L9 17L9 7Z" fill="#currentColor"/>
+      <path d="M9 7L15 12L9 17L9 7Z" fill="#currentColor" />
     </Icon>
   );
 }
